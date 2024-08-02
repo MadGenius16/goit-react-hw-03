3f5929f1230a30f84290f73c9a8b7a0c12719e76
MadGenius16 <aleksandrco777@gmail.com>
2024-08-02 22:36:54 +0300

    Start

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import css from "./App.module.css"
+import ContactForm from "./ContactForm/ContactForm"
+import ContactList from "./ContactList"
+import SearchBox from "./SearchBox/SearchBox"
+import contacts from "./contacts.json"
+
+
+function App() {
+
+
+  return (
+    <div className={css.container}>
+      <h1>Phonebook</h1>
+      <ContactForm/>
+      <SearchBox/>
+      <ContactList/>
+    </div>
+  )
+}
+
+export default App
